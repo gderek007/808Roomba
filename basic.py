3a629c4e429126f902adceeeb9cb0a32fe88bc1e
Derek Garcia <39495257+gderek007@users.noreply.github.com>
2022-05-06 12:26:16 -0400

    More Structure for our program
    Added todos and more structure to the code

--- basic.py
+++ basic.py
@@ -1,3 +1,4 @@
+from ast import Pass
 import mercury
 #From this library https://github.com/gotthardp/python-mercuryapi
 from  pycreate2 import Create2
@@ -17,6 +18,7 @@ for i in tags:
 # {300833B2DDD9014000000000 , E28011700000020A7B2E8D44, E200001D5607014724307EB8 , E28011700000020F4CB328F0, 
 # E2004740D42BFE4A755E1887, E2000020780A003117906133, E2801170000002118067C4E5, E20043B7034FB3891A4627D1}
 
+#dict might be useful to map name to RSSI? not too sure tho 
 tag_dictionary = {}
 total_tags_names, identified_tags  = set() , set()
 total_tags_names = {'300833B2DDD9014000000000' , 'E28011700000020A7B2E8D44', 'E200001D5607014724307EB8' , 'E28011700000020F4CB328F0', \
@@ -39,32 +41,47 @@ while (True):
     if (len(identified_tags) == len(total_tags_names)) :
         break
     if abs( closest_tag.rssi ) < 55 :
-        if ( getTagName(closest_tag) == (wanted_tag) ) :
+        # TODO might have to change this approach to just grab any tag not specifically the wanted tag
+        # just check if the tag has been discovered already
+        if ( getTagName(closest_tag) not in identified_tags ) :
             identified_tags.add( wanted_tag )
             tags_left = total_tags_names - identified_tags
             # gets a random tag from our wanted set
             wanted_tag = r.sample(tags_left, 1) [0]
-            #clean for some amount of time
-            #code for cleaning around the RFID Tag
+            # clean for some amount of time
+            # TODO code for cleaning around the RFID Tag
     else :
         previous_reading = abs(closest_tag.rssi)
-        # rotate robot CW
-        # do some sort of reading and moving until the rssi goes up
+        # TODO rotate robot CW
+        # TODO do some sort of reading and moving until the rssi goes up
+        old_tag = closest_tag
+        closest_tag = getClosestTag()
+        # TODO not sure how yet but veryify that we arent going after a tag we have seen already, cause would waste time
+        if (closest_tag == old_tag) :
+            pass
+        else:
+            # tag changed? make sure dont move
+            time.sleep(1)
+            closest_tag = getClosestTag()
         post_reading = abs(closest_tag.rssi)
         delta = post_reading - previous_reading
         # positive delta means new reading is worst than old reading, therefore rotate CCW
         if delta > 0 :
             #might be last time to rotate right so drive forward somehow
-            #rotate left
-            #command to rotate for some amount of seconds
+            # TODO rotate left
+            # TODO command to rotate for some amount of seconds
             pass
         else :
             #rotate right some more to get more readings
-            #rotate right
-            #command to rotate for some amount of seconds
+            #TODO rotate right
+            #TODO command to rotate for some amount of seconds
             pass
 
+
+print("Done")
+# TODO set up for the Creat2 Robot
 # Create a Create2 Robot
+# ls on dev and choose usbserial-XXXXXXXX
 # port = "/dev/tty.usbserial-DN0289CJ"  
 # bot = Create2(port)
 # bot.start()
@@ -76,9 +93,11 @@ while (True):
 # time.sleep(2)
 # bot.drive_stop()
 
+# TODO potential alternative to async read on the fly (might be more difficult)
 # asynchronous reading set up
 # reader.start_reading(lambda tag: print("Name: " + str(tag.epc)[2:-1], "RSSI: " + str(tag.rssi)))
 
+# v random code not helpful i belive 
 # def nearest_tag(tag):
 #     rssi = tag.rssi
 #     if 
