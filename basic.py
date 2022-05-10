ce25c3983472128e891d2b6647657ce9ff1ddfad
Derek Garcia <derekgar@mit.edu>
2022-05-10 19:48:02 -0400

    incorporates walls and turning backwards

--- basic.py
+++ basic.py
@@ -7,21 +7,31 @@ from  pycreate2 import Create2
 import time
 import random as r
 #ls on dev and choose cu.usbmodemXXXXXX
-reader = mercury.Reader("tmr:///dev/cu.usbmodem14201")
+reader = mercury.Reader("tmr:///dev/cu.usbmodem141301")
 reader.set_region("EU3")
 reader.set_read_plan([4], "GEN2")
 
+#afdgb
+#abdfg
 # RFID Tag Names Names
 # {300833B2DDD9014000000000 , E28011700000020A7B2E8D44, E200001D5607014724307EB8 , E28011700000020F4CB328F0, 
 # E2004740D42BFE4A755E1887, E2000020780A003117906133, E2801170000002118067C4E5, E20043B7034FB3891A4627D1}
 
 #dict might be useful to map name to RSSI? not too sure tho 
-tag_dictionary = {'E2004740D42BFE4A755E1887': 'A', 'E28011700000020F4CB328F0':'B', 'E200001D5607014724307EBD':'C', '300833B2DDD9014000000000':'D', \
-    'E2801170000002118067C4E5':'E', 'E20043B7034FB3891A4627D1':'F', 'E2000020780A003117906133':'G', 'E28011700000020A7B2E8D44':'H'}
+# tag_dictionary = {'E2004740D42BFE4A755E1887': 'A', 'E28011700000020F4CB328F0':'B', 'E200001D5607014724307EBD':'C', '300833B2DDD9014000000000':'D', \
+#     'E2801170000002118067C4E5':'E', 'E20043B7034FB3891A4627D1':'F', 'E2000020780A003117906133':'G', 'E28011700000020A7B2E8D44':'H'}
+# tag_dictionary_misses = {'E2004740D42BFE4A755E1887': 0, 'E28011700000020F4CB328F0': 0, 'E200001D5607014724307EBD': 0, '300833B2DDD9014000000000': 0, \
+#     'E2801170000002118067C4E5': 0, 'E20043B7034FB3891A4627D1': 0, 'E2000020780A003117906133': 0, 'E28011700000020A7B2E8D44': 0}
 total_tags_names, identified_tags  = set() , set()
 # ordered ABC from our sticky notes, A-H
-total_tags_names = {'E2004740D42BFE4A755E1887' , 'E28011700000020F4CB328F0', 'E200001D5607014724307EBD', '300833B2DDD9014000000000', \
-    'E2801170000002118067C4E5', 'E20043B7034FB3891A4627D1', 'E2000020780A003117906133', 'E28011700000020A7B2E8D44'}
+# total_tags_names = {'E2004740D42BFE4A755E1887' , 'E28011700000020F4CB328F0', '300833B2DDD9014000000000', \
+#     'E20043B7034FB3891A4627D1', 'E2000020780A003117906133' }
+tag_dictionary = {'E2004740D42BFE4A755E1887': 'A', 'E28011700000020F4CB328F0':'B', '300833B2DDD9014000000000':'D', \
+ 'E20043B7034FB3891A4627D1':'F', 'E2000020780A003117906133':'G' }
+tag_dictionary_misses = {'E2004740D42BFE4A755E1887': 0, 'E28011700000020F4CB328F0': 0, '300833B2DDD9014000000000': 0, \
+    'E20043B7034FB3891A4627D1': 0, 'E2000020780A003117906133': 0}
+total_tags_names = {'E2004740D42BFE4A755E1887' , 'E28011700000020F4CB328F0', '300833B2DDD9014000000000', \
+    'E20043B7034FB3891A4627D1', 'E2000020780A003117906133'}
 closest_tag, wanted_tag = 0 , 'E2004740D42BFE4A755E1887'
 ## ROBOT SETUP:
 ## set port to usb serial value 
@@ -72,7 +82,7 @@ while (True):
         break
     
     if closest_tag == -1 :
-        bot.drive_direct(-100, -100)
+        # bot.drive_direct(-100, -100)
         time.sleep(1)
         bot.drive_stop()
         continue
@@ -85,10 +95,10 @@ while (True):
             #Tag found
             if ( getTagName(closest_tag) not in identified_tags ) :
                 #(250,125) (200,100)
-                bot.drive_direct(-100, -100)
-                time.sleep(0.5)
-                bot.drive_direct(-50, -100)
-                time.sleep(0.5)
+                bot.drive_direct(-200, -200)
+                time.sleep(2)
+                bot.drive_direct(-100, -200)
+                time.sleep(2)
                 print("FOUND: ", getTagName(closest_tag), getTagLabel(closest_tag), closest_tag.rssi)
                 identified_tags.add( getTagName(closest_tag) )
                 tags_left = total_tags_names - identified_tags
@@ -108,21 +118,32 @@ while (True):
                     break
                 else:
                     cond = True
-            # New tag has to be claimed
+            # New tag has to be claimed because old one is out of range.
             if cond and i == len(tags) - 1: 
                 print ("tag was not found, no Delta")
+                desired_tag = getTagName(wanted_tag)
+                tag_dictionary_misses[desired_tag] += 1
+                print(tag_dictionary_misses)
                 break
-            post_reading = abs(wanted_tag.rssi)
 
+            post_reading = abs(wanted_tag.rssi)
             delta = post_reading - previous_reading
             # positive delta means new reading is worst than old reading, therefore rotate CCW
+            sensors = bot.get_sensors()
+            sensors.wall == sensors[1]  # True
+            if (sensors.wall) :
+                bot.drive_direct(-300, -300)
+                time.sleep(1)
+                bot.stop()
+                bot.drive_direct(-150, 150)
+                time.sleep(.5)
+                bot.stop()
+
             while (delta ==  0):
-                delta = r.randint(-2 , 2)
+                delta = r.randint(-1 , 1)
             if delta > 0 :
                 print("Delta > 0 ", delta)
-                #might be last time to rotate right so drive forward somehow
-                # TODO rotate left
-                # TODO command to rotate for some amount of seconds
+                #rotate left some more to get more readings
                 if abs(delta) > 2 :
                     bot.drive_direct(45, 0)
                 elif abs(delta) > 4 :
@@ -134,8 +155,6 @@ while (True):
             elif delta < 0 :
                 print("Delta < 0", delta)
                 #rotate right some more to get more readings
-                #TODO rotate right
-                #TODO command to rotate for some amount of seconds
                 if abs(delta) > 2 :
                     bot.drive_direct(0, 45)
                 elif abs(delta) > 4 :
@@ -146,37 +165,3 @@ while (True):
                 bot.drive_stop()
 
 print("Done")
-#clean up one last time??
-
-# TODO set up for the Creat2 Robot
-# Create a Create2 Robot
-# ls on dev and choose usbserial-XXXXXXXX
-# port = "/dev/tty.usbserial-DN0289CJ"  
-# bot = Create2(port)
-# bot.start()
-# bot.safe()
-# bot.full()
-# bot.drive_direct(100, 100)
-# time.sleep(2)
-# bot.drive_direct(200,-200)  # inputs for motors are +/- 500 max
-# time.sleep(2)
-# bot.drive_stop()
-
-# TODO potential alternative to async read on the fly (might be more difficult)
-# asynchronous reading set up
-# reader.start_reading(lambda tag: print("Name: " + str(tag.epc)[2:-1], "RSSI: " + str(tag.rssi)))
-
-# v random code not helpful i belive 
-# def nearest_tag(tag):
-#     rssi = tag.rssi
-#     if 
-# def stats_received(stats):
-#     print({"temp" : stats.temperature})
-#     print({"antenna" : stats.antenna})
-#     print({"protocol" : stats.protocol})
-#     print({"frequency" : stats.frequency})
-#     print(stats)
-
-# reader.enable_stats(stats_received)
-#time.sleep(1)
-#reader.stop_reading()
